f903cef6654a21c467ee5b7beca30013b70d3334
Matt Jewell <m@mattjewell.co.uk>
2021-12-02 13:33:46 +0100

    2020 day 17 complete

--- src/main/kotlin/adventofcode/y2020/Day17.kt
+++ src/main/kotlin/adventofcode/y2020/Day17.kt
@@ -1,21 +1,25 @@
 package adventofcode.y2020
 
+import java.io.File
+
 fun main() {
     val day2 = Day17()
-    println("Example part one: ${day2.examplePart1()} cubes")
+    println("Example 1 answer: ${day2.examplePart1()} cubes")
+    println("Part 1 answer: ${day2.part1()} cubes")
+    println("Example 2 answer: ${day2.examplePart2()} cubes")
+    println("Part 2 answer: ${day2.part2()} cubes")
 }
 
 class Day17 {
-    fun examplePart1(): Int {
-        val world = World.fromInput(exampleInput)
-        return world.afterCycles(6).activeCubes.size
-    }
+    fun examplePart1(): Int = afterCycles(cubes3DFromStrings(exampleInput), 6).size
+    fun part1(): Int = afterCycles(cubes3DFromStrings(puzzleInput), 6).size
+    fun examplePart2(): Int = afterCycles(cubes4DFromStrings(exampleInput), 6).size
+    fun part2(): Int = afterCycles(cubes4DFromStrings(puzzleInput), 6).size
 
-    data class Cube(val x: Int, val y: Int, val z: Int) {
+    abstract class Cube {
         fun willBeActive(otherCubes: Set<Cube>): Boolean {
             val wasActive = otherCubes.contains(this)
             val alive = neighbors().count { it != this && otherCubes.contains(it) }
-
 
             return if (wasActive) {
                 alive == 2 || alive == 3
@@ -24,43 +28,53 @@ class Day17 {
             }
         }
 
+        abstract fun neighbors(): Set<Cube>
+    }
 
+    data class Cube3D(val x: Int, val y: Int, val z: Int) : Cube() {
         // includes the cube itself
-        fun neighbors(): Set<Cube> =
+        override fun neighbors(): Set<Cube> =
             (-1..1).flatMap { dX ->
                 (-1..1).flatMap { dY ->
                     (-1..1).map { dZ ->
-                        Cube(this.x + dX, this.y + dY, this.z + dZ)
+                        Cube3D(this.x + dX, this.y + dY, this.z + dZ)
                     }
                 }
             }.toSet()
     }
 
-    class World(val activeCubes: Set<Cube>) {
-        companion object {
-            fun fromInput(input: List<String>): World {
-                return World(cubesFromStrings(input))
-            }
-
-            private fun cubesFromStrings(input: List<String>) =
-                input.flatMapIndexed { indexY: Int, str: String ->
-
-                    str.mapIndexedNotNull { indexX, c ->
-                        if (c == '#') Cube(indexX, indexY, 0) else null
+    data class Cube4D(val x: Int, val y: Int, val z: Int, val w: Int) : Cube() {
+        // includes the cube itself
+        override fun neighbors(): Set<Cube> =
+            (-1..1).flatMap { dX ->
+                (-1..1).flatMap { dY ->
+                    (-1..1).flatMap { dZ ->
+                        (-1..1).map { dW ->
+                            Cube4D(this.x + dX, this.y + dY, this.z + dZ, this.w + dW)
+                        }
                     }
-                }.toSet()
-        }
-
-        fun afterCycles(i: Int): World {
-            println("activeCubes.groupBy { it.third }  ${activeCubes.groupBy { it.z }}")
-            val nextActive = activeCubes.flatMap { activeCube ->
-                activeCube.neighbors().mapNotNull { cube -> cube.takeIf { it.willBeActive(activeCubes) } }
+                }
             }.toSet()
+    }
 
-            
+    fun cubes3DFromStrings(input: List<String>) =
+        input.flatMapIndexed { indexY: Int, str: String ->
+            str.mapIndexedNotNull { indexX, c ->
+                if (c == '#') Cube3D(indexX, indexY, 0) else null
+            }
+        }.toSet()
 
-            return World(emptySet())
-        }
+    fun cubes4DFromStrings(input: List<String>) =
+        input.flatMapIndexed { indexY: Int, str: String ->
+            str.mapIndexedNotNull { indexX, c ->
+                if (c == '#') Cube4D(indexX, indexY, 0, 0) else null
+            }
+        }.toSet()
+
+    tailrec fun afterCycles(activeCubes: Set<Cube>, remainingCycles: Int): Set<Cube> {
+        if (remainingCycles == 0) return activeCubes
+        val nextCubes = activeCubes.flatMap { it.neighbors() }.toSet().filter { it.willBeActive(activeCubes) }.toSet()
+        return afterCycles(nextCubes, remainingCycles - 1)
     }
 }
 
@@ -70,3 +84,6 @@ private val exampleInput = """
     ..#
     ###
 """.trimIndent().split("\n")
+
+
+private val puzzleInput = File("aoc-input/day17.txt").readLines()
