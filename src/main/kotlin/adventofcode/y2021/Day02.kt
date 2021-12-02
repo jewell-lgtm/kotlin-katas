d797d9e36be1f19470d9844a12fb4fc920d4f03a
Matt Jewell <m@mattjewell.co.uk>
2021-12-02 07:41:38 +0100

    day2 part 2

--- src/main/kotlin/adventofcode/y2021/Day02.kt
+++ src/main/kotlin/adventofcode/y2021/Day02.kt
@@ -3,37 +3,60 @@ package adventofcode.y2021
 fun main() {
     val day02 = Day02()
 
-    println("The example gives position ${day02.example1Position()} with product ${day02.example1Position().product()}")
-    println("Part One gives position ${day02.part1Position()} with product ${day02.part1Position().product()}")
+    println("Example 1 gives position ${day02.example1Position()} with product ${day02.example1Position().product()}")
+    println("Part 1 gives position ${day02.part1Position()} with product ${day02.part1Position().product()}")
+    println("Example 2 gives position ${day02.example2Position()} with product ${day02.example2Position().product()}")
+    println("Part 2 gives position ${day02.part2Position()} with product ${day02.part2Position().product()}")
 
 
 
 }
 
 class Day02 {
+    fun example1Position() = move2D(parseInput(exampleInput1), Position2D(0, 0))
+    fun part1Position() = move2D(parseInput(puzzleInput), Position2D(0, 0))
+    fun example2Position() = moveAim(parseInput(exampleInput2), PositionAim(0, 0, 0))
+    fun part2Position() = moveAim(parseInput(puzzleInput), PositionAim(0, 0, 0))
+
     enum class Command {
         FORWARD, DOWN, UP
     }
 
-    data class Position(val h: Int, val depth: Int) {
-        operator fun plus(input: Pair<Command, Int>): Position {
+    data class Position2D(val h: Int, val depth: Int) {
+        operator fun plus(input: Pair<Command, Int>): Position2D {
             val (dir, amount) = input
             return when (dir) {
-                Command.FORWARD -> Position(h + amount, depth)
-                Command.DOWN -> Position(h, depth + amount)
-                Command.UP -> Position(h, (depth - amount).coerceAtLeast(0))
+                Command.FORWARD -> Position2D(h + amount, depth)
+                Command.DOWN -> Position2D(h, depth + amount)
+                Command.UP -> Position2D(h, (depth - amount).coerceAtLeast(0))
             }
         }
 
         fun product(): Int  = h * depth
     }
 
-    fun example1Position(): Position = move(parseInput(exampleInput1), Position(0, 0))
-    fun part1Position(): Position = move(parseInput(puzzleInput), Position(0, 0))
+    data class PositionAim(val h: Int, val depth: Int, val aim: Int) {
+        operator fun plus(input: Pair<Command, Int>): PositionAim {
+            val (dir, amount) = input
+            return when (dir) {
+                Command.FORWARD -> PositionAim(h + amount, depth + (aim * amount), aim)
+                Command.DOWN -> PositionAim(h, depth, aim + amount)
+                Command.UP -> PositionAim(h, depth, aim - amount)
+            }
+        }
 
-    private tailrec fun move(input: List<Pair<Command, Int>>, position: Position): Position {
+        fun product(): Int  = h * depth
+    }
+
+    private tailrec fun move2D(input: List<Pair<Command, Int>>, position: Position2D): Position2D {
         if (input.isEmpty()) return position
-        return move(input.drop(1), position + input.first())
+
+        return move2D(input.drop(1), position + input.first())
+    }
+
+    private tailrec fun moveAim(input: List<Pair<Command, Int>>, position: PositionAim): PositionAim {
+        if (input.isEmpty()) return position
+        return moveAim(input.drop(1), position + input.first())
     }
 
     private val regex = """(.+) (\d+)""".toRegex()
@@ -58,6 +81,15 @@ class Day02 {
 private val puzzleInput = getInput("Day02")
 
 private val exampleInput1 = """
+    forward 5
+    down 5
+    forward 8
+    up 3
+    down 8
+    forward 2
+""".trimIndent().split("\n")
+
+private val exampleInput2 = """
     forward 5
     down 5
     forward 8
