67ae29abfe1171f527eeeffb0f6275b3f831aa7f
Matt Jewell <m@mattjewell.co.uk>
2021-12-05 08:21:53 +0100

    my day template

--- src/main/kotlin/adventofcode/y2021/DayXX.kt
+++ src/main/kotlin/adventofcode/y2021/DayXX.kt
@@ -1,10 +1,23 @@
 package adventofcode.y2021
 
 fun main() {
-    println(puzzleInput.size)
+    val day = DayXX()
+    println("Example 1 answer: ${day.example1()}")
+    println("Part 1 answer: ${day.puzzle1()}")
+    println("Example 2 answer: ${day.example2()}")
+    println("Part 2 answer: ${day.puzzle2()}")
 
 
 
 }
 
+private class DayXX {
+    fun example1 (): Int = exampleInput.size
+    fun puzzle1 (): Int = puzzleInput.size
+    fun example2 (): Int = exampleInput.size
+    fun puzzle2 (): Int = puzzleInput.size
+}
+
 private val puzzleInput = getInput("Day01")
+private val exampleInput = """
+""".trimIndent().split("\n")
