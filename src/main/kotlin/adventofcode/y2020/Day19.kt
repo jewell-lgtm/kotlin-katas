e39da60949556b2e760933e189b69d345e3632c5
Matt Jewell <m@mattjewell.co.uk>
2021-12-08 06:37:41 +0100

    Day 19 was interrupted

---
+++ src/main/kotlin/adventofcode/y2020/Day19.kt
@@ -0,0 +1,25 @@
+package adventofcode.y2020
+
+import java.io.File
+
+fun main() {
+    val day = Day19()
+
+
+
+}
+
+class Day19 {
+
+}
+
+
+private val exampleInput1 = """
+    0: 1 2
+    1: "a"
+    2: 1 3 | 3 1
+    3: "b"
+""".trimIndent().split("\n")
+
+
+private val puzzleInput = File("aoc-input/day19.txt").readLines()
