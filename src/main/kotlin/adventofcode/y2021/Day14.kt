940d19be1efaa65fd5b609d124f2520ed8ed1392
Matt Jewell <m@mattjewell.co.uk>
2021-12-14 14:11:43 +0100

    puzzle 14 part 2

--- src/main/kotlin/adventofcode/y2021/Day14.kt
+++ src/main/kotlin/adventofcode/y2021/Day14.kt
@@ -3,37 +3,64 @@ package adventofcode.y2021
 fun main() {
     val day = Day14()
 
-    val example2 = day.example2()
-    val puzzle2 = day.puzzle2()
-
-    println("Example 1 answer: ${day.example1()}")
+    println("Example 1: ${day.example1()}")
     println("Part 1 answer: ${day.puzzle1()}")
-    println("Example 2 answer: $example2")
-    println("Part 2 answer: $puzzle2")
+    println("Example 2 answer: ${day.example2()}")
+    println("Part 2 answer: ${day.puzzle2()}")
 }
 
 private class Day14 {
-    fun example1(): Int = run(parseInput(exampleInput), 10)
-    fun puzzle1(): Int = run(parseInput(puzzleInput), 10)
-    fun example2(): Int = run(parseInput(exampleInput), 40)
-    fun puzzle2(): Int = run(parseInput(puzzleInput), 40)
+    fun example1(): Long = run(parseInput(exampleInput), 10)
+    fun puzzle1(): Long = run(parseInput(puzzleInput), 10)
+    fun example2(): Long = run(parseInput(exampleInput), 40)
+    fun puzzle2(): Long = run(parseInput(puzzleInput), 40)
 
 
-    private fun run(input: PuzzleInput, generations: Int): Int {
+    private fun run(input: PuzzleInput, generations: Int): Long {
         val rules = input.rules
-        val substrate = input.substrate
-        val charCount: MutableMap<Char, Int> = mutableMapOf()
-        charCount.inc(substrate.toCountMap())
+        val substrate = input.substrate.toMutableList()
+        val result = substrate.toMutableCountMap()
+        val memo = mutableMapOf<Triple<Int, Char, Char>, Map<Char, Long>>()
 
-        repeat(generations) { count ->
-            println("count $count")
-            val added = substrate.mutateNext(rules)
-            charCount.inc(added)
+        while (substrate.size > 1) {
+            val start = substrate.removeFirst()
+            val next = substrate[0]
+            result.accumulate(
+                charsBetween(memo, generations, rules, start, next)
+            )
         }
 
-        val result = charCount.mce() - charCount.lce()
-        println("result ${result}")
-        return result
+
+        return result.mce() - result.lce()
+    }
+
+
+    private fun charsBetween(
+        memo: MutableMap<Triple<Int, Char, Char>, Map<Char, Long>>,
+        maxDepth: Int,
+        rules: Set<Rule>,
+        start: Char,
+        end: Char,
+    ): Map<Char, Long> {
+
+        fun charsBetween(depth: Int, start: Char, end: Char, acc: MutableMap<Char, Long>): Map<Char, Long> {
+            if (depth == maxDepth) return acc
+            val memoKey = Triple(depth, start, end)
+            if (!memo.containsKey(memoKey)) {
+                val inserted = rules.firstNotNullOf { it.match(start, end) }
+
+                memo[memoKey] = mutableMapOf(inserted to 1L)
+                    .accumulate(
+                        charsBetween(depth + 1, start, inserted, acc)
+                    ).accumulate(
+                        charsBetween(depth + 1, inserted, end, acc)
+                    )
+            }
+            return memo[memoKey]!!
+        }
+
+
+        return charsBetween(0, start, end, mutableMapOf())
     }
 
 
@@ -43,7 +70,7 @@ private class Day14 {
         return PuzzleInput(substrate.toMutableList(), rules)
     }
 
-    data class PuzzleInput(val substrate: MutableList<Char>, val rules: Set<Rule>)
+    data class PuzzleInput(val substrate: List<Char>, val rules: Set<Rule>)
     data class Rule(val first: Char, val second: Char, val insert: Char) {
         fun match(a: Char, b: Char): Char? = if (a == first && b == second) insert else null
 
@@ -57,6 +84,7 @@ private class Day14 {
         }
     }
 
+    // first implementation
     private fun MutableList<Char>.mutateNext(rules: Set<Rule>): Map<Char, Int> {
         val result = mutableMapOf<Char, Int>()
         var i = size - 1
@@ -73,22 +101,21 @@ private class Day14 {
         return result
     }
 
+    private fun <E> List<E>.toMutableCountMap(): MutableMap<E, Long> = toCountMap().toMutableMap()
 
-    private fun <K> MutableMap<K, Int>.inc(other: Map<K, Int>) {
-        other.forEach { (key, value) ->
-            this[key] = getOrDefault(key, 0) + value
-        }
-    }
-
-    private fun <E> MutableList<E>.toCountMap(): Map<E, Int> =
+    private fun <E> List<E>.toCountMap(): Map<E, Long> =
         fold(mutableMapOf()) { acc, e -> acc.also { it[e] = acc.getOrDefault(e, 0) + 1 } }
 
-    private fun <K> MutableMap<K, Int>.mce() =
+    private fun <K> MutableMap<K, Long>.mce() =
         maxByOrNull { it.value }!!.value
 
-    private fun <K> MutableMap<K, Int>.lce() =
+    private fun <K> MutableMap<K, Long>.lce() =
         minByOrNull { it.value }!!.value
 
+    private fun <K> MutableMap<K, Long>.accumulate(other: Map<K, Long>): MutableMap<K, Long> {
+        other.forEach { (key, value) -> this[key] = this.getOrDefault(key, 0) + value }
+        return this
+    }
 
 }
 
